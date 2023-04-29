c48a77f6d40d6c0ad3d3e1d56d424de36cf2e2c9
Sahil Arora <91737464+Sahil-2309@users.noreply.github.com>
2023-04-29 14:08:33 +0530

    changed

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../../index.css'
+import './experience.css'
 import { AiFillHtml5 } from 'react-icons/ai'
 import { DiCss3 } from 'react-icons/di'
 import { BsFillBootstrapFill } from 'react-icons/bs'
 import { FaReact } from 'react-icons/fa'
-import { SiTailwindcss, SiMongodb, SiMysql } from 'react-icons/si'
+import { SiTailwindcss, SiMongodb } from 'react-icons/si'
 import { IoLogoJavascript } from 'react-icons/io'
 import { FaPython } from 'react-icons/fa'
+import { GrMysql } from 'react-icons/gr'
 const experience = () => {
   return (
     <section id='experience'>
@@ -16,41 +18,73 @@ const experience = () => {
         <div className='experience__frontend'>
           <h3>Frontend Development</h3>
           <div className='experience__content'>
-            <article className='experience__detials'>
-              <AiFillHtml5 />
-              <h4>HTML</h4>
-              <small className='text-light'>Experienced</small>
-              <DiCss3 />
-              <h4>CSS</h4>
-              <small className='text-light'>Intermediate</small>
-              <BsFillBootstrapFill />
-              <h4>Bootstrap</h4>
-              <small className='text-light'>Experienced</small>
-              <FaReact />
-              <h4>ReactJS</h4>
-              <small className='text-light'>Experienced</small>
-              <SiTailwindcss />
-              <h4>Tailwind</h4>
-              <small className='text-light'>Experienced</small>
-              <IoLogoJavascript />
-              <h4>Javascript</h4>
-              <small className='text-light'>Experienced</small>
+            <article className='experience__details'>
+              <AiFillHtml5 className='experience__deatils-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <DiCss3 className='experience__deatils-icon' />
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsFillBootstrapFill className='experience__deatils-icon' />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <FaReact className='experience__deatils-icon' />
+              <div>
+                <h4>ReactJS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <SiTailwindcss className='experience__deatils-icon' />
+              <div>
+                <h4>Tailwind</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <IoLogoJavascript className='experience__deatils-icon' />
+              <div>
+                <h4>Javascript</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </article>
           </div>
         </div>
         <div className='experience__backend'>
           <h3>Backend Development</h3>
           <div className='experience__content'>
-            <article className='experience__detials'>
-              <FaPython />
-              <h4>Python</h4>
-              <small className='text-light'>Intermediate</small>
-              <SiMongodb />
-              <h4>MongoDB</h4>
-              <small className='text-light'>Intermediate</small>
-              <SiMysql />
-              <h4>MySQL</h4>
-              <small className='text-light'>Intermediate</small>
+            <article className='experience__details'>
+              <FaPython className='experience__deatils-icon' />
+              <div>
+                <h4>Python</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <SiMongodb className='experience__deatils-icon' />
+              <div>
+                <h4>MongoDB</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <GrMysql className='experience__deatils-icon' />
+              <div>
+                <h4>MySQL</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
             </article>
           </div>
         </div>
