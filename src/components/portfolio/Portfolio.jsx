e9488f7d8bbc101e500c6996ae0867df4da8bd37
Sahil Arora <91737464+Sahil-2309@users.noreply.github.com>
2023-05-01 23:20:43 +0530

    added jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
 import '../../index.css'
-
+import './portfolio.css'
+import IMG1 from '../../assets/project1/Screenshot 2023-05-01 211811.png'
+import IMG2 from '../../assets/project2/Screenshot 2023-05-01 211429.png'
 const Portfolio = () => {
-  return <section id='portfolio'>Portfolio</section>
+  return (
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt='' />
+          </div>
+          <h3>MOVIE RECOMMENDER SYSTEM</h3>
+          <a
+            href='https://github.com/Sahil-2309/Movie-Recommender'
+            className='btn btn-primary'
+          >
+            Github
+          </a>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG2} alt='' />
+          </div>
+          <h3>PERSONAL PORTFOLIO WEBSITE</h3>
+          <a
+            href='https://github.com/Sahil-2309/Movie-Recommender'
+            className='btn btn-primary'
+          >
+            Github
+          </a>
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default Portfolio
