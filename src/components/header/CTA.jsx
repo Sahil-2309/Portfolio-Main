42f8601f0af895f689e216554e310ba163341978
Sahil Arora <91737464+Sahil-2309@users.noreply.github.com>
2023-05-02 10:37:44 +0530

    deployable

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import Sahil from '../../assets/Sahil.pdf'
 import '../../index.css'
+
 const CTA = () => {
   return (
-    <>
-      <div className='cta'>
-        <a href={Sahil} download className='btn' rel='noreferrer'>
-          Download CV
-        </a>
-        <a href='#contact' className='btn btn-primary' rel='noreferrer'>
-          Lets's Talk
-        </a>
-      </div>
-    </>
+    <div className='cta'>
+      <a href={Sahil} download className='btn' rel='noreferrer'>
+        Download CV
+      </a>
+      <a href='#contact' className='btn btn-primary' rel='noreferrer'>
+        Let's Talk
+      </a>
+    </div>
   )
 }
 
