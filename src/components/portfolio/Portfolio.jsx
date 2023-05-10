f95e8335a562d3204c4715dba4eddb20ae6574ff
Sahil Arora <91737464+Sahil-2309@users.noreply.github.com>
2023-05-11 01:02:45 +0530

    added photos

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '../../index.css'
 import './portfolio.css'
 import IMG1 from '../../assets/project1/Main.png'
-import IMG2 from '../../assets/project2/Screenshot 2023-05-01 211429.png'
+import IMG2 from '../../assets/project2/Main.png'
 const Portfolio = () => {
   return (
     <section id='portfolio'>
